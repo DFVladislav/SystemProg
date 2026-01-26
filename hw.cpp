287c907caa530276097068c0327393afed88f807
DFVladislav <fedya12346598@mail.ru>
2026-01-26 12:37:22 +0000

    Zadania

---
+++ hw.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello world! \n"; 
+    std::cerr << "ERROR \n";
+    int a;
+    std::cin >> a;
+    return a;
+}
---
+++ sockets.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(int argC, char* argV[12]) {
+    for (int i = 0; 0 < argC; argV++) {
+        argV[i] = 0;
+    }
+    for (int i =0; 0 < argC; argV++) {
+    } 
+    std::cout << "arg" << i
+    return 0;
+}
