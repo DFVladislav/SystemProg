bbb7eba54c8b7ab62e855e252c85537ef7d70f62
DFVladislav <fedya12346598@mail.ru>
2026-04-13 09:07:46 +0000

    commites

---
+++ bfsdfs.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+void printQueue (std::queue <int> q , const std::string& label)
+{
+    std::cout<<label<<" [";
+    bool first = true;
+    while (!q.empty())
+    {
+        if (!first){std::cout << " " ; }
+        std::cout << q.front();
+        q.pop ();
+        first = false;
+        std::cout << " ]";
+    }
+}
+
+class Graph 
+{
+    int numVertices;
+    std::vector < std::vector < int >> adj;
+    std::vector < bool > visited;
+    bool verbose;
+
+    public:
+    Graph (int V, bool verboseMode = false)
+    {
+        numVertices = V,
+        adj.resize(V);
+        visited.resize (V , false);
+        verbose = verboseMode;
+    }
+
+    void addEdge (int u, int v)
+    {
+        adj[u].push_back(v);
+        adj[v].push_back(v);
+    }
+
+    void BFS(int startVertex)
+    {
+        std::fill(visited.begin() , visited.end(), false);
+        std::queue<int>q;
+        visited [startVertex] = true;
+        q.push (startVertex);
+        if (verbose) 
+        {
+            printQueue (q,"asdasd");
+        }
+        std::cout << "посещенные вершины";
+        int step = 1;
+        int level = 0;
+
+        while(!q.empty())
+        {
+            if(verbose) { ; }
+            int currentVertex = q.front();
+            q.pop();
+            if (verbose) { ; }
+            std::cout << currentVertex << " ";
+            if (verbose){ ; } // *
+            for (int neighbor:adj[currentVertex])
+            {
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    q.push (neighbor);
+                    if (verbose) {;}
+                }
+            }
+            if (verbose) { printQueue (q,"sdfsdf") ; }
+            step++;
+        } // while
+        std::cout << std::endl;
+        std::cout << "END \n";
+
+    }//BFS
+};//Graph
+
+int main(){
+    return 0;
+
+
+    Graph g1(6, true)
+
+    g1.addEdge(0,1)
+    g1.BFS()
+}
